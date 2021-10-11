75b63f53beb688cd80ea131af898269d128a3c95
FilipeJohansson <filipejohansson7@gmail.com>
2021-10-11 10:18:46 -0300

    putting toolbar bottom & styling title input

--- public/RTE.php
+++ public/RTE.php
@@ -6,8 +6,7 @@
     <title>Full Editor - Quill Rich Text Editor</title>
     <!-- Include stylesheet -->
     <!-- Bootstrap v5.0 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="../src/css/quill.css" title="article">
     <link rel="stylesheet" href="../src/css/style.css" title="article">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -17,8 +16,8 @@
 <body>
     <header id="" class="py-1 px-5 fixed-top d-flex align-items-center">
         <div class="col-9">
+            <input type="text" autocomplete="off" id="inputTitle" class="form-control d-inline" aria-describedby="titleHelpBlock" required>
             <label for="inputTitle" class="form-label">Título</label>
-            <input type="text" id="inputTitle" class="form-control d-inline" aria-describedby="titleHelpBlock">
             <div id="titleHelpBlock" class="form-text">
                 Máximo 100 caracteres
             </div>
@@ -38,20 +37,21 @@
                     <input type="file" accept=".png, .jpg, .jpeg" class="form-control" id="inputArticleImage" aria-label="Upload">
                 </div>
 
-
                 <div class="pb-3">
                     <label for="inputDescription" class="form-label">Descrição</label>
-                    <textarea type="text" id="inputDescription" rows="3" class="form-control"
-                        aria-describedby="descriptionHelpBlock"></textarea>
+                    <textarea type="text" id="inputDescription" rows="3" class="form-control" aria-describedby="descriptionHelpBlock"></textarea>
                     <div id="descriptionHelpBlock" class="form-text">
                         Máximo 300 caracteres
                     </div>
                 </div>
 
+                <label class="form-label">Escreva seu artigo</label>
                 <div id="article-editor">
-                    <div id="editor" class="p-3">
-                        O massa é que essa lista foi indicação de um queridão seguidor do meu canal do YouTube chamado
-                        Paulo Camargo, então muito obrigado Paulo!
+                    <div id="editor" class="p-3 mb-4">
+                        <p>
+                            O massa é que essa lista foi indicação de um queridão seguidor do meu canal do YouTube chamado
+                            Paulo Camargo, então muito obrigado Paulo!
+                        </p>
 
 
 
@@ -130,12 +130,63 @@
                         do:
                     </div>
                 </div>
+
+                <div id="toolbar" class="fixed-bottom">
+                    <span class="ql-formats">
+                    </span>
+                    <span class="ql-formats">
+                        <span class="ql-formats">
+                            <button class="ql-header" value="" label="Normal" selected>Normal</button>
+                        </span>
+                        <span class="ql-formats">
+                        </span>
+                        <span class="ql-formats">
+                            <button class="ql-header" value="2" label="Subtítulo">Subtítulo</button>
+                        </span>
+                    </span>
+
+                    <span class="ql-formats">
+                        <button class="ql-bold"></button>
+                        <button class="ql-italic"></button>
+                        <button class="ql-underline"></button>
+                        <button class="ql-blockquote"></button>
+                    </span>
+
+                    <span class="ql-formats">
+                        <button class="ql-link"></button>
+                        <button class="ql-image"></button>
+                        <button class="ql-video"></button>
+                    </span>
+
+                    <span class="ql-formats">
+                        <select class="ql-align">
+                            <option selected></option>
+                            <option value="center"></option>
+                            <option value="right"></option>
+                            <option value="justify"></option>
+                        </select>
+                    </span>
+
+                    <span class="ql-formats">
+                        <button class="ql-list" value="ordered"></button>
+                        <button class="ql-list" value="bullet"></button>
+                    </span>
+
+                    <span class="ql-formats">
+                        <button class="ql-indent" value="+1"></button>
+                        <button class="ql-indent" value="-1"></button>
+                    </span>
+
+                    <span class="ql-formats">
+                        <button class="ql-clean"></button>
+                    </span>
+                </div>
             </section>
         </div>
     </main>
 
     <script>
-        $('body').on('click', '#publish', function () {
+        $('body').on('click', '#publish', function() {
             const title = document.getElementById('inputTitle').value
             const description = document.getElementById('inputDescription').value
             const editor = document.getElementsByClassName('ql-editor')
@@ -148,7 +199,7 @@
                     description: description,
                     editor: editor[0].innerHTML
                 },
-                success: function (result) {
+                success: function(result) {
                     //console.log(result)
                 }
             });
@@ -163,27 +214,28 @@
         var Delta = Quill.import('delta');
         var quill = new Quill('#editor', {
             modules: {
-                toolbar: [
-                    [{
-                        header: [2, false]
-                    }],
-                    ['bold', 'italic', 'underline', 'blockquote'],
-                    ['link', 'image', 'video'],
-                    [{
-                        'align': []
-                    }],
-                    [{
-                        'list': 'ordered'
-                    }, {
-                        'list': 'bullet'
-                    }],
-                    [{
-                        'indent': '-1'
-                    }, {
-                        'indent': '+1'
-                    }],
-                    ['clean']
-                ]
+                toolbar: '#toolbar'
+                /*[
+                                   [{
+                                       header: [2, false]
+                                   }],
+                                   ['bold', 'italic', 'underline', 'blockquote'],
+                                   ['link', 'image', 'video'],
+                                   [{
+                                       'align': []
+                                   }],
+                                   [{
+                                       'list': 'ordered'
+                                   }, {
+                                       'list': 'bullet'
+                                   }],
+                                   [{
+                                       'indent': '-1'
+                                   }, {
+                                       'indent': '+1'
+                                   }],
+                                   ['clean']
+                               ]*/
             },
             scrollingContainer: '#scrolling-container',
             placeholder: 'Escreva aqui seu artigo',
@@ -192,12 +244,12 @@
 
         // Store accumulated changes
         var change = new Delta();
-        quill.on('text-change', function (delta) {
+        quill.on('text-change', function(delta) {
             change = change.compose(delta);
         });
 
         // Save periodically
-        setInterval(function () {
+        setInterval(function() {
             if (change.length() > 0) {
                 console.log('Saving changes', change);
                 /* 
@@ -216,7 +268,7 @@
         }, 5 * 1000);
 
         // Check for unsaved data
-        window.onbeforeunload = function () {
+        window.onbeforeunload = function() {
             if (change.length() > 0) {
                 return 'There are unsaved changes. Are you sure you want to leave?';
             }
